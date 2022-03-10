0e9f710db6ecc47efc55611f09630cc09a6cbfe1
Miguel Teheran <miguelteheran@gmail.com>
2022-03-10 00:31:30 -0500

    db in memory

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
+using FundamentosEFPlatzi;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TareaContext>(opt => opt.UseInMemoryDatabase("TareaDB"));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbconection", async ([FromServices] TareaContext dbContext) =>
+    {
+        dbContext.Database.EnsureCreated();
+        return Results.Ok("Database created:" + dbContext.Database.IsInMemory());
+    });
 
 app.Run();
