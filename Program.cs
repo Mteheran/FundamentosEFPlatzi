f70bf9f53ab08b4fe4a1049537e5d70827836573
Miguel Teheran <miguelteheran@gmail.com>
2022-03-10 14:55:23 -0500

    connecting to real database

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 using FundamentosEFPlatzi;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<TareaContext>(opt => opt.UseInMemoryDatabase("TareaDB"));
+//builder.Services.AddDbContext<TareaContext>(opt => opt.UseInMemoryDatabase("TareaDB"));
+builder.Services.AddSqlServer<TareaContext>("Data Source=server;Initial Catalog=db;user id=sa; password=pass");
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
