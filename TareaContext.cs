14b67175f7b15d66d7da99893fb0ac4a088634c0
Miguel Teheran <miguelteheran@gmail.com>
2022-03-10 20:21:50 -0500

    fluentApicategorias

--- TareaContext.cs
+++ TareaContext.cs
@@ -6,5 +6,20 @@ public class TareaContext : DbContext
 {
     DbSet<Categoria> Categorias {get;set;}
     DbSet<Tarea> Tareas {get;set;}
-    public TareaContext(DbContextOptions<TareaContext> options) : base(options) { }        
+    public TareaContext(DbContextOptions<TareaContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Categoria>(categoria=>
+        {
+            categoria.ToTable("Categoria");
+
+            categoria.HasKey(p=> p.CategoriaId);
+
+            categoria.Property(p=> p.Nombre).IsRequired().HasMaxLength(150);
+
+            categoria.Property(p=> p.Descripcion);
+        });       
+
+    }        
 }
